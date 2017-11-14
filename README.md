3b5abb357e0221b0da05196fc5fd03d30b507df1
root <root@lygontv.lxd>
2017-11-14 21:40:42 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # node-salt-api
-A simple Node.js package to send functions to Salt Stack salt-api via CherryPi
+A simple Node.js package to send functions to Salt Stack salt-api via CherryPy.  
+Use Salt modules from Node.js.
+
+## Install
+
+_TODO_
+
+## Example
+
+```
+const Salt = require("salt-api");
+const salt = salt({
+	username: "salt",
+	password: "secret"
+});
+
+salt.ready.then(() => {
+
+	// Same as running `salt "*" test.ping` in the command line
+	salt.fun("*", "test.ping").then(data => {
+
+		// Do something with the data
+		console.log(data);
+		// { return: [ { b827eb3aaaf7: true, b827ebcc82fe: true } ] }
+
+	}).catch(e => console.error(e));
+
+});
+```
+
+> LICENSE: MIT
---
+++ salt.js
@@ -0,0 +1,50 @@
+const request = require("request-promise-native");
+
+class Salt {
+	constructor(config) {
+		this.config = config;
+		this.headers = "";
+		this.init();
+	}
+
+	init() {
+		this.ready = request({
+			url: this.config.url + "/login",
+			method: "POST",
+			json: true,
+			form: {
+				username: this.config.username,
+				password: this.config.password,
+				eauth: (typeof this.config.eauth === "string")?this.config.eauth:"pam"
+			}
+		}).then(data => {
+			if(typeof data === "object" && typeof data.return === "object" && typeof data.return[0].token === "string") {
+				this.token = data.return[0].token;
+				this.expire = data.return[0].expire;
+			} else {
+				throw "Got no token";
+			}
+		}).catch(e => console.error(e));
+	}
+
+	fun(tgt="*", fun="test.ping", client="local") {
+		return request({
+			url: this.config.url,
+			method: "POST",
+			json: true,
+			headers: {"X-Auth-Token": this.token},
+			form: { tgt, fun, client }
+		});
+	}
+
+	testToken() {
+		return new Promise( async resolve => {
+			if(this.expire <= new Date() / 1000) {
+				await this.init();
+			}
+		});
+	}
+
+}
+
+module.exports = Salt;
